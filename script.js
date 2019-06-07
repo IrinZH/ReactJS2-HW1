570ef30a5fb7cf951b8279d3b25905ac1bbb1f2a
IrinZh <irishinskin@gmail.com>
2019-06-07 18:18:16 +0300

    последняя версия

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const employers = ['Alex', '', 'ludmila', 'Viktor', '', ' oleg ', 'iNna  ', 'Iva
 let employersNames = employers.filter( name => name.length > 0);
     employersNames = employersNames.map( name => name.toLowerCase().trim());
 
-let own = 0; //для четвертого варианта
+// let own = 0; //для четвертого варианта
 
 const sponsors = {
     cash: [40000, 5000, 30400, 12000],
@@ -37,16 +37,35 @@ return total;
 // let money = calcCash(); //третий вариант
 
 //четвертый вариант
-const calcCash = (own, [...cash]) => {
+/* const calcCash = (own, [...cash]) => {
     let total = own;
     let cashItem = cash.reduce((previous, current) => {
         return previous + current;
     }); 
     total += +cashItem; 
     return total;
-}
+} */
 
-let money = calcCash(0, sponsors.cash); 
+// let money = calcCash(0, sponsors.cash); //четвертый вариант
+
+//пятый вариант
+/* const {cash} = sponsors;
+
+function calcCash(...cash){
+    let total = 0
+    cash.forEach((item)=>total+=item);
+    return total
+}
+let money = calcCash(0, ...cash); */
+
+const {cash} = sponsors;
+const calcCash = (...cash) => {
+    let cashItem = cash.reduce((previous, current) => {
+        return previous + current;
+    });
+    return cashItem;
+}
+let money = calcCash(0, ...cash);
 
 const makeBusiness = (owner, director = 'Victor', cash, emp) => {
     const {eu, rus} = sponsors;
